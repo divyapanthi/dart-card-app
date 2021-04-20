2251e9703c2b20839cc5d6e746d3474c7b56473d
Divya <deevyapanthi03@gmail.com>
2021-04-20 08:14:40 +0545

    Added remove card method and enhanced the mulitiline coding into fewer lines.

--- cardApp.dart
+++ cardApp.dart
@@ -1,181 +1,113 @@
-void main(){
+void main() {
   var deck = new Deck();
-  print(deck);
+
+//   print(deck);
+
+//   deck.printCards();
+
+//   deck.shuffleCards();
+//   deck.printCards();
+//   var hand = deck.deal(6);
+//   print(hand);
+//   deck.printCards();
+  
+  var cards = deck.cardsWithSuit(color: 'Clubs');
+  print(cards);
+  
+  deck.printCards();
+  deck.removeCard("Ace", suit: "Spade");
   deck.printCards();
 }
 
-//this is a collection of cards
-class Deck{
-  //collection of 52 playing cards
-  List<Card> cards=[];
-  
-  Deck() {
-    //create 52 cards and assign it to the cards field
-    List<String> ranks = ["Ace","Two","Three","Four","Five","Six"];
-    List<String> suits = ["Spade","Clubs","Diamonds","Hearts"];
-    
-    //nested for loop
-    
-//     for(int i=0; i<suits.length; i++){
-      
-//       for(int j=0; j<ranks.length; j++){
-//         //create a new Card and add it to cards
-//         var card = new Card(suits[i], ranks[j]);
-//         cards.add(card);  
-//       }
-     
-//     }
-    
-    
-    for(var suit in suits){
-      for(var rank in ranks){
-        var card = new Card(suit, rank);
-        cards.add(card); 
-      }
-      
-    }
-    
-  }
-  
-  void printCards() {
-    print(cards);
-  }
-  
-  @override
-  String toString(){
-    return "";
-  }
-  
-}
-
-
-//individual card
-class Card{
-  //represents the rank such as Ace, Queen, Jack
-  String rank;
-  //represents the suit such as Space, Club
-  String suit;
-  
-  Card(this.suit, this.rank);
-  
-  @override 
-  String toString(){
-    return "$rank of $suit";
-  }
-}
-
-
-
-
-void main () {
-  var deck = new Deck();
- 
-//   print(deck);
-  
-//   deck.printCards();
-//   deck.shuffleCards();
-  
-//   print("\n");
-//   deck.printCards();
-  
-//   var hand = deck.deal(6);
-//   print("\n");
-//   print(hand);
-  
-//   print("\n");
-//   deck.printCards();
-  
-  var cards= deck.cardsWithSuit('Diamonds');
-  print(cards);
-  
-}
 // this is a collection of cards
 class Deck {
   // collection of 52 playing cards
   List<Card> cards = [];
-  
-  Deck () {
+
+  Deck() {
     //create 52 cards and assign it to the cards field
-    List<String> ranks = ["Ace", "Two","Three","Four","Five","Six"];
-    List<String> suits = ["Spade","Clubs","Hearts","Diamonds"];
-    
+    List<String> ranks = ["Ace", "Two", "Three", "Four", "Five", "Six"];
+    List<String> suits = ["Spade", "Clubs", "Hearts", "Diamonds"];
+
     // nested for loop
 //     for( int i = 0; i< suits.length; i++) {
 //       for(int j =0; j< ranks.length; j++ ){
 //         //create a new Card and add it to cards
 //         var card = new Card(suits[i], ranks[j]);
-//         cards.add(card);
-        
+//        cards.add(card);
+
 //       }
 //     }
-    for(var suit in suits){
-      for(var rank in ranks){
-        
-        var card = new Card(suit,rank);
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(rank:rank, suit:suit);
         cards.add(card);
-        
       }
     }
 //     for(int i= 0;i<suits.length; i++){
 //       var suit = suits[i];
 //     }
   }
-  
+
   void printCards() {
     print(cards);
   }
-  
-  shuffleCards(){
+
+  void shuffleCards() {
     cards.shuffle();
   }
-  
-  deal(int size){
-    var hand = cards.sublist(0,size);
-    cards= cards.sublist(size);
+
+  List<Card> deal(int size) {
+    var hand = cards.sublist(0, size);
+    cards = cards.sublist(size);
     return hand;
   }
-  
-  cardsWithSuit(String color){
-    List<Card> matched = [];
+
+  List<Card> cardsWithSuit({String color}) {
+//     List<Card> matched = [];
+
+//     for (var card in cards) {
+//       if (card.suit == color) {
+//         matched.add(card);
+//       }
+//     }
+//     return matched;
     
-    for(var card in cards){
-      if(card.suit == color){
-        matched.add(card);
-      }
-    }
-    return matched;
+    
+//       var matched = cards.where( (card) {
+//         return card.suit == color;
+//       });
+//       return matched;
+    
+//         var matched = cards.where( (singleCard) => singleCard.suit == color );
+//         return matched;
+    
+        return cards.where( (singleCard) => singleCard.suit == color).toList();
+    
+    
   }
   
+ 
+  void removeCard(String rank, {String suit}){
+      // todo: remove the card which matches the supplied rank and suit
+      
+      cards.removeWhere((singleCard) => (singleCard.suit == suit) && (singleCard.rank == rank));
+     
+    }
   
-//   @override
-//   String toString() {
-//     return "It is a deck";
-//   }
-  
-  
+ 
 }
+
 class Card {
   // represents the rank such as Ace, Queen, Jack
   String rank;
   // represents the suit such as Spade, Club
   String suit;
-  
-  Card(this.suit, this.rank);
-  
+
+  Card({this.suit, this.rank});
+
   @override
- String toString(){
-   return "$rank of $suit";
+  String toString() {
+    return "$rank of $suit";
   }
-  
-  
 }
-
-
-
-
-
-
-
-
-
-
